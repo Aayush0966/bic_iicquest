c89efe1d59c0a9f013024dbc2da2d1f3e0c6d650
Aayush Budhathoki <161975799+Aayush0966@users.noreply.github.com>
2025-06-12 22:43:42 +0545

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,9 +21,7 @@ const io = initializeSocket(server);
 
 // Middleware
 app.use(cors({
-  origin: "*",
-  credentials: true
-}));
+  origin: "*"}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
